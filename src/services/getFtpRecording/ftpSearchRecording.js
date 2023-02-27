5f59ac31ba04897578e5143c590aeb3abce7d1d0
Juan.Ixcopal <juan.ixcopal@alumnos.uneatlantico.es>
2023-02-27 18:17:24 +0100

    Feature: List Files to FTP

--- src/services/getFtpRecording/ftpSearchRecording.js
+++ src/services/getFtpRecording/ftpSearchRecording.js
@@ -1,18 +1,27 @@
 import Client from 'ftp'
+import fs from 'fs'
 
 function listFtpDirectory(config) {
   return new Promise((resolve, reject) => {
     var c = new Client()
 
     c.on('ready', function () {
-      c.list('../../var/spool/asterisk/monitor/2023/02/23/internal-1001-1000-20230223-211217-1677186737.4.wav', function (err, list) {
+      c.list('../../var/spool/asterisk/monitor/2023/02/23', function (err, list) {
         if (err) throw err
         else {
           console.log('Grabaciones', list)
-          c.end()
           resolve(list)
+          c.end()
         }
       })
+
+      // c.get('../../var/spool/asterisk/monitor/2023/02/23/internal-1000-1001-20230223-210819-1677186499.2.wav', function (err, stream) {
+      //   if (err) throw err
+      //   stream.once('close', function () {
+      //     c.end()
+      //   })
+      //   stream.pipe(fs.createWriteStream('Grabacion1.wav'))
+      // })
     })
     c.connect(config)
   })
@@ -25,7 +34,7 @@ export default function makeFtpSearchRecording({ configFtp }) {
       // aquí puedes hacer lo que necesites con la lista de archivos
       return list
     } catch (error) {
-      console.log('Eror', error)
+      console.log('Error', error)
     }
   }
 }
